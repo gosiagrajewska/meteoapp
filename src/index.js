df7b9536b435eebec9dbdb56af8bb96b058d17e5
gosiagrajewska <149269502+gosiagrajewska@users.noreply.github.com>
2023-12-23 17:36:22 +0100

    added api integration city and current temp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 function showCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#searched-city");
-  cityElement.innerHTML = searchInput.value;
-}
 
-let searchCity = document.querySelector("#search-form");
-searchCity.addEventListener("submit", showCity);
+  searchCity(searchInput.value);
+}
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+function refreshWeather(response) {
+  let currentTemp = document.querySelector("#current-temperature");
+  currentTemp.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#searched-city");
+  cityElement.innerHTML = response.data.city;
+}
+let searchedCity = document.querySelector("#search-form");
+searchedCity.addEventListener("submit", showCity);
+searchCity("Warsaw");
